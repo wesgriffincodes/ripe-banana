d04c4491144150e5c12afa80bbd01a51653fe0e3
Wes Griffin <codingclueless@gmail.com>
2019-07-19 17:50:13 -0700

    reviewer getById and test pass

--- test/reviewer.test.js
+++ test/reviewer.test.js
@@ -4,7 +4,11 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Film = require('../lib/models/Film');
+const Studio = require('../lib/models/Studio');
+const Actor = require('../lib/models/Actor');
 const Reviewer = require('../lib/models/Reviewer');
+const Review = require('../lib/models/Review');
 
 describe('reviewer routes', () => {
   beforeAll(() => {
@@ -13,6 +17,19 @@ describe('reviewer routes', () => {
 
   beforeEach(() => {
     return mongoose.connection.dropDatabase();
+  });
+  
+  let studio = null;
+  let actor = null;
+  let film = null;
+  let reviewer = null;
+  let review = null;
+  beforeEach(async() => {
+    studio = JSON.parse(JSON.stringify(await Studio.create({ name: 'universal' })));
+    actor = JSON.parse(JSON.stringify(await Actor.create({ name: 'Ben Kingsley'  })));
+    film = JSON.parse(JSON.stringify(await Film.create({ title: 'Captain Ron', studio: studio._id, released: 1993, cast: [{ actor: actor._id, role: 'Captain' }] })));
+    reviewer = JSON.parse(JSON.stringify(await Reviewer.create({ name: 'bobby bling', company: 'who cares' })));
+    review = JSON.parse(JSON.stringify(await Review.create({ rating: 3, reviewer: reviewer._id, review:'another review', film: film._id })));
   });
 
   afterAll(() => {
@@ -45,6 +62,28 @@ describe('reviewer routes', () => {
         const reviewersJSON = JSON.parse(JSON.stringify(reviewers));
         reviewersJSON.forEach(reviewer => {
           expect(res.body).toContainEqual({ name: reviewer.name, company: reviewer.company, _id: reviewer._id });
+        });
+      });
+  });
+
+  it('GET reviewer by id', async() => {
+    return request(app)
+      .get(`/api/v1/reviewers/${reviewer._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: reviewer._id,
+          name: reviewer.name,
+          company: reviewer.company,
+          reviews: [{
+            _id: review._id,
+            rating: review.rating,
+            review: review.review,
+            film: {
+              _id: film._id,
+              title: film.title
+            }
+          }]
+
         });
       });
   });
